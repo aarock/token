815a2cbce6ff5c1b2c514cc8ee4a258aac0aa06d
Aaron Gale <aaron.gale@eyemagnet.com>
2023-04-06 19:47:29 +1000

    fix: header access errors on unauthenticated routes

--- src/pluck.ts
+++ src/pluck.ts
@@ -6,8 +6,8 @@ export default function pluck ( ctx: any ): string {
 		|| ctx?.headers?.Authorization
 		|| ctx?.headers?.authorization
 		// as a yoga + graphql-ws subscription param
-		|| ctx?.params.extensions.headers?.Authorization
-		|| ctx?.params.extensions.headers?.authorization
+		|| ctx?.params?.extensions?.headers?.Authorization
+		|| ctx?.params?.extensions?.headers?.authorization
 		// as a get request query parameter
 		|| ctx?.req?.query?.token
 		|| ctx?.query?.token
